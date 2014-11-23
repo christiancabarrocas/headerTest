b8c681d4a6d5a1884484ee0f4b810690e8344be5
Christian Cabarrocas <ccabarrocas@MacBook-Pro-de-Christian.local>
2014-11-23 10:02:26 +0100

    Header animating working version

--- headerViewControlleTableViewController.swift
+++ headerViewControlleTableViewController.swift
@@ -10,13 +10,17 @@ import UIKit
 
 class headerViewControlleTableViewController: UITableViewController,UIScrollViewDelegate {
 
-    private let kTableHeaderHeight:CGFloat = 300.0
-    private let ktableHeaderCutAway:CGFloat = 80.0
-    var headerView:UIView!
-    var headerMaskLayer:CAShapeLayer
-    
     let items = [
     
+        NewsItem(category: .World, summary:"Climate changes protests"),
+        NewsItem(category: .Europe, summary:"Sctoland's Yes to independence shakes Europe"),
+        NewsItem(category: .MiddleEast, summary:"Next we use the if let control structure"),
+        NewsItem(category: .Africa, summary:"Making it optional will allow a value of nil when the cell is first created"),
+        NewsItem(category: .Americas, summary:"We will need to change the table view data source methods"),
+        NewsItem(category: .Asia, summary:"UITableView, cellForRowAtIndexPath indexPath: NSIndexPath, we have to retrieve the NewsItem value from items corresponding to the row number of the table view cell to display"),
+        NewsItem(category: .Europe, summary:"Sctoland's Yes to independence shakes Europe"),
+        NewsItem(category: .World, summary:"Climate changes protests"),
+        NewsItem(category: .Americas, summary:"We will need to change the table view data source methods"),
         NewsItem(category: .World, summary:"Climate changes protests"),
         NewsItem(category: .Europe, summary:"Sctoland's Yes to independence shakes Europe"),
         NewsItem(category: .MiddleEast, summary:"Next we use the if let control structure"),
@@ -28,32 +32,45 @@ class headerViewControlleTableViewController: UITableViewController,UIScrollView
         NewsItem(category: .Americas, summary:"We will need to change the table view data source methods")
     ]
     
+    private let kTableHeaderHeight:CGFloat = 300.0
+    private let ktableHeaderCutAway:CGFloat = 50.0
+    var headerView:UIView!
+    var headerMaskLayer:CAShapeLayer!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 50;
+        setupTableHeader()
+    }
+
+    func setupTableHeader () {
+        headerView = tableView.tableHeaderView
+        tableView.tableHeaderView = nil
+        tableView.addSubview(headerView)
+        let effectiveHeight = kTableHeaderHeight-ktableHeaderCutAway/2
+        tableView.contentInset = UIEdgeInsets(top: effectiveHeight , left: 0, bottom: 0, right: 0)
+        tableView.contentOffset = CGPointMake(0, -effectiveHeight)
         
         headerMaskLayer = CAShapeLayer()
         headerMaskLayer.fillColor = UIColor.blackColor().CGColor
         headerView.layer.mask = headerMaskLayer
-        headerView = tableView.tableHeaderView
-        tableView.tableHeaderView = nil
-        tableView.addSubview(headerView)
-        tableView.contentInset = UIEdgeInsetsMake(kTableHeaderHeight, 0, 0, 0)
-        tableView.contentOffset = CGPointMake(0, -kTableHeaderHeight)
+        
         updateHeaderView()
-    }
 
-    override func scrollViewDidScroll(scrollView: UIScrollView) {
+    }
+    
+    override  func scrollViewDidScroll(scrollView: UIScrollView) {
         updateHeaderView()
     }
     
     func updateHeaderView() {
-        var headerRect = CGRectMake(0, -kTableHeaderHeight, tableView.bounds.width, kTableHeaderHeight)
-        if tableView.contentOffset.y < -kTableHeaderHeight {
+        let effectiveHeight = kTableHeaderHeight-ktableHeaderCutAway/2
+        var headerRect = CGRectMake(0, -effectiveHeight, tableView.bounds.width, kTableHeaderHeight)
+        if tableView.contentOffset.y < -effectiveHeight {
             headerRect.origin.y = tableView.contentOffset.y
-            headerRect.size.height = -tableView.contentOffset.y
+            headerRect.size.height = -tableView.contentOffset.y + ktableHeaderCutAway/2
         }
         headerView.frame = headerRect
         
@@ -62,23 +79,27 @@ class headerViewControlleTableViewController: UITableViewController,UIScrollView
         path.addLineToPoint(CGPointMake(headerRect.width, 0))
         path.addLineToPoint(CGPointMake(headerRect.width, headerRect.height))
         path.addLineToPoint(CGPointMake(0, headerRect.height-ktableHeaderCutAway))
-        headerMaskLayer.path = path.CGPath
+        headerMaskLayer?.path = path.CGPath
     }
     
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
     
+    
     // MARK: - TABLE VIEW
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
     }
+
     override func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
         return 1
     }
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("customCell", forIndexPath: indexPath) as CustomCell
+
         let item = items[indexPath.row]
         cell.newsItem = item
 
